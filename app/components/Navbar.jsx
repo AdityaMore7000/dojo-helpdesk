37b9839d6e1b2aee9444b18d34dd9da7c57bff34
adityamore7000 <adi.more7000@gmail.com>
2023-12-22 12:45:11 +0530

    complete

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,9 +1,11 @@
+import Image from "next/image";
 import Link from "next/link";
-
+import Logo from './dojo-logo.png'
 export default function Navbar() {
   return (
     <>
         <nav>
+          <Image src={Logo} alt="ninja-dojo-logo" width={70} quality={100} placeholder="blur"/>
           <h1>Dojo Help Desk</h1>
           <Link href={'/'}>Dashboard</Link>
           <Link href={'/tickets'}>Tickets</Link>
